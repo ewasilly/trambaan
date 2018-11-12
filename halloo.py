9e6ea1c0f5bec980b5fc5922d77d49a9376440e3
louiseAB97 <louisebuijs@gmail.com>
2018-11-12 15:54:19 +0100

    Aantekeningen tech assistance

---
+++ halloo.py
@@ -0,0 +1,20 @@
+IDEA points!
+
+infrastructuur
+datastructuur
+aan de hand van voorbeeld github!
+
+files inlezen
+
+classes definieeren en zorgen dat het niet allemaal hetzelfde doet
+
+
+
+github cheatsheet
+git pull
+git add .
+git status
+git push
+git commit -m "message"
+
+We werken NIET met branches
