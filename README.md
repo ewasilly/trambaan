0e0c981a1a83f82f7d9a5e2a12120cf85ec17890
louiseAB97 <louisebuijs@gmail.com>
2018-11-26 17:45:48 +0100

    Objective function geimplementeerd, en counter voor tellen hoevaak een connectie gebruikt wordt

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 Mijn Project
 [ Korte beschrijving van de RailNL case]
 
+RailNL heeft recentelijk een doelfunctie opgesteld voor de kwaliteit van de lijnvoering. Als 100% van van de kritieke verbindingen bereden wordt, levert dat 10000 punten op je lijnvoering op, anders krijg je een een gedeelte daarvan. Maar hoe minder trajecten voor dezelfde service, hoe goedkoper. En in hoe minder tijd er in al die trajecten samen verbruikt wordt, hoe beter. Dus die factoren worden ook meegewogen in de doelfunctie:
+
+
+K = p*10000 - (T*20 + Min/10)
+
+waarin K de kwaliteit van de lijnvoering is, p de fractie van de bereden kritieke verbindingen (dus tussen 0 en 1), T het aantal trajecten en Min het aantal minuten in alle trajecten samen.
+
+
+
 Statespace:
 
 u = (s * c_1 * (c_2)^a_m)^t
--- Trajectgenerator_versiesdupl.py
+++ Trajectgenerator_versiesdupl.py
@@ -33,6 +33,10 @@ print(f"De opgestelde trajecten zijn: {trajects}")
 """
 Versie 2: Hierin kun je een minimale lengte voor een traject instellen. Als je 50 min doet lukt het prima,
 bij 80 min rekent hij al langer omdat de incidentie van een random opgesteld traject dat zo lang duurt klein is.
+
+Heuristieken nog toepassen:
+kritieke verbindingen mogen dubbel gebruikt worden,
+niet kriteike niet.
 """
 
 i = 1
@@ -69,3 +73,61 @@ while i < 7:
                 break
 
 print(f"De opgestelde trajecten zijn: {trajects}")
+
+
+"""
+versie 3 incl objective function variables
+"""
+# create random array of numbers to use as indices in all_connections
+numbers = np.arange(10000)
+trajects = {}
+
+# OPTION: choose between critical or all connections
+connections = all_connections
+print(connections)
+print(connections[4])
+
+# count the use of connections during all 7 trajects
+used_all = []
+# keep track of total time, will be used in objective function
+total_minutes = []
+# keep track of used critical connections
+used_critical = []
+
+# the length of all_connections can be used with modulo. More info later.
+length = len(connections)
+
+i = 1
+# arbitrary :
+min_time = 50
+while i < 7:
+    # generate a random number to use as starting connection in Traject
+    start = random.choice(range(length))
+    traject = Traject(connections[start])
+    # # Prevent going back and forth by keeping track of used connections
+    # used = []
+    tried = 1
+    while traject.total_time <= 120:
+        # modulo is used to prevent "index out of range list" error.
+        j = random.choice(numbers)
+        index = j % length
+        # using the Traject method add_connection, to add the connection at [j] in all_connections
+        if connections[index] not in traject.connections:
+            traject.add_connection(connections[index])
+            tried += 1
+        # if every connection has been tried:
+        if tried == length:
+            # if the total time of the current traject overceeds min_time use this traject.
+            if traject.total_time >= min_time:
+                trajects[f"Traject{i}."] = traject
+                total_minutes.append(traject.total_time)
+                i += 1
+                # add connections in this traject to used_all, eventually the use through all 7 trajects will be counted
+                for conn in traject.connections:
+                    used_all.append(conn)
+                    if conn in critical_connections:
+                        used_critical.append(conn)
+                break
+            # if the traject is too short, start over.
+            else:
+                break
