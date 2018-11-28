1de5109f9780e499669d41eebbb364514cdb74e9
louiseAB97 <louisebuijs@gmail.com>
2018-11-28 16:37:12 +0100

    lief berichtje voor Jasperge

--- mainLou.py
+++ mainLou.py
@@ -109,8 +109,6 @@ Create trajects
 # Note on connections; choose critical_connections OR all_connections
 def traject_generator_greedy(connections, nr_of_trajects, min_time):
 
-    # create random array of numbers to use for indexing through connections
-    numbers = np.arange(10000)
     trajects = {}
     # count the use of connections during all trajects
     used_all = []
@@ -121,65 +119,61 @@ def traject_generator_greedy(connections, nr_of_trajects, min_time):
     # length of connections = the amount of connections in this list.
     length = len(connections)
 
+    counter = collections.Counter(connections)
+
     i = 1
     while i < nr_of_trajects:
-        # generate a random number to use as starting connection in Traject
+        # generate a random starting connection for the Traject
         start = random.choice(range(length))
         traject = Traject(connections[start])
-        # # Prevent going back and forth by keeping track of used connections
-        # used = []
-        tried = 1
+        tried = []
+        random.shuffle(connections)
         while traject.total_time <= 120:
-            # modulo is used to prevent "index out of range list" error.
-            j = random.choice(numbers)
-            index = j % length
-            # using the Traject method add_connection, to add the connection at [j] in all_connections
-            if connections[index] not in traject.connections:
-                traject.add_connection(connections[index])
-                tried += 1
-            # if every connection has been tried:
-            if tried == length:
-                # if the total time of the current traject overceeds min_time use this traject.
-                if traject.total_time >= min_time:
-                    trajects[f"Traject {i}."] = (traject, traject.total_time)
-                    total_minutes.append(traject.total_time)
-                    i += 1
-                    # add connections in this traject to used_all, eventually the use through all 7 trajects will be counted
-                    for conn in traject.connections:
-                        used_all.append(conn)
-                        if conn in critical_connections:
-                            used_critical.append(conn)
-                    break
-                # if the traject is too short, start over.
-                else:
-                    break
+            for index in range(length):
+                # add connection if not yet in traject and not yet used more than 3 times
+                if connections[index] not in traject.connections and counter[connections[index]] <= 2:
+                    traject.add_connection(connections[index])
+            # if the total time of the current traject overceeds min_time use this traject.
+            if traject.total_time >= min_time:
+                trajects[f"Traject {i}."] = (traject, traject.total_time)
+                total_minutes.append(traject.total_time)
+                i += 1
+                # add connections in this traject to used_all, eventually the use through all 7 trajects will be counted
+                for conn in traject.connections:
+                    used_all.append(conn)
+                    counter = collections.Counter(used_all)
+                    if conn in critical_connections:
+                        used_critical.append(conn)
+                break
+            # if the traject is too short, start over.
+            else:
+                break
 
-    p = len(used_critical) / len(critical_connections)
+
+
+    p = len(collections.Counter(used_critical)) / len(critical_connections)
     t = len(trajects.keys())
     total_minutes = sum(total_minutes)
     K = p*10000 - (t*20 + total_minutes/10)
-
+    f= len(counter)/len(connections)
+    print(p)
+    print(f"Greedy approach trajects output: {trajects} \n")
+    print(f"Fraction: {f}\n")
+    print(f"K: {K}\n")
+    print(counter)
     return(K)
 
-    # print(f"Greedy approach trajects output: {trajects} \n")
-    # # Counter lists the use of all connections
-    # counter = collections.Counter(used_all)
-    # print(f"Counter: {counter}\n")
+
 
 
 K_distribution = []
-for i in range(100):
-    K = traject_generator_greedy(all_connections, 7, 90)
+for i in range(500):
+    K = traject_generator_greedy(all_connections, 6, 60)
     K_distribution.append(K)
 
-print(K_distribution)
-plt.hist(K_distribution, bins='auto')  # arguments are passed to np.histogram
-plt.title("K spread - 100 iterations - 6 trajects, 90 minutes max")
+plt.hist(K_distribution, bins='auto')
+plt.title("K spread - 500 iterations - 5 trajects, min 50 minutes")
 plt.show()
-
-
-
-
 
 
 # K de kwaliteit van de lijnvoering is, p de fractie van de bereden kritieke verbindingen (dus tussen 0 en 1),
