54880047d2bb01a3ed6d146ceaeab7e2403b2a26
Ewa <ewa_sillem@hotmail.com>
2018-12-07 16:00:35 +0100

    README neat

--- mainEwaNew.py
+++ mainEwaNew.py
@@ -106,45 +106,60 @@ print(f"all connections: {all_connections}")
 """
 Create trajects
 """
+def K_calculator(trajects):
+
+    used_conns = []
+    used_crit = []
+    total_minutes = []
+    for traject in trajects:
+        total_minutes.append(traject.total_time)
+        for conn in traject.connections:
+            used_conns.append(conn)
+            if conn in critical_connections:
+                used_crit.append(conn)
+
+    # fraction of used connections
+    f= len(collections.Counter(used_conns))/len(all_connections)
+
+    p = len(collections.Counter(used_crit))/ len(critical_connections)
+    t = len(traject.connections)
+    total_minutes = sum(total_minutes)
+    K = p*10000 - (t*20 + total_minutes/10)
+
+    # print(f"F: {f}")
+    # print(f"P: {p}\n")
+    # print(f"K: {K}")
+
+    return(K)
+
 
 traject = Traject
 # Note on connections; choose critical_connections OR all_connections
 def traject_generator_depth(connections):
 
-    # create random array of numbers to use for indexing through connections
-    numbers = np.arange(10000)
-    trajects = {}
-    # count the use of connections during all trajects
-    used_all = []
-    # keep track of total time, will be used in objective function
-    total_minutes = []
-    # keep track of used critical connections
-    used_critical = []
-    # length of connections = the amount of stations in this list.
-    length = len(stations_dict)
-
-
     # List to save the possible connections from a certain station
     possibilities = []
     possible_time = []
+    length = len(connections)
 
     # generate a random number to use as starting station in Traject
-    #start = random.choice(range(length))
-    start = 2
+    start = random.choice(range(length))
+    # start = 2
+    dict_trajects = {}
 
     # Ensure that no more than 7 trajects are made
-    # Don't forget to change 1 to 7!!!
+    # Don't forget to change 1 to 7!
     i = 0
     time = 0
     while i < 1:
         # while loop to make one traject
-        while time < 120:
+        while time < 100:
             possibilities.clear()
             possible_time.clear()
             # search for the possible connections from start
             for connection in all_connections:
                 # if start is starting point of a connection, add to possibilities
-                if start == connection.id_from:
+                if start == connection.id_from: 
                     possibilities.append(connection)
 
             # search for the possible connection with the shortest time
@@ -152,103 +167,56 @@ def traject_generator_depth(connections):
                 possible_time.append(connection.time)
 
             if len(possibilities) == 0:
+                start = random.choice(range(length))
                 break
 
             min_time = min(possible_time)
-
-            print(f"possibilities: {possibilities}")
-            print(f"possible_time: {possible_time}")
-            print(f"min_time: {min_time}")
-
-            # make dictionary per station
-            for value in stations_dict:
-                if value != "True" and value != "False":
-                    value = {}
 
 
             for connection in possibilities:
                 if connection.time == min_time:
                     if i == 0:
                         traject = Traject(connection)
+                        # add station to dictionary
+                        # name_key = stations_dict[connection.id_from][0]
+                        # print(name_key)
+
                         time = time + connection.time
-                        start = connection.id_from
-                        stop = connection.id_to
-                        dict_temp2 = {}
-                        dict_temp = {stop: dict_temp2}
-                        dict = {start : dict_temp}
                         start = connection.id_to
                     else:
                         traject.add_connection(connection)
                         time = time + connection.time
-                        start = connection.id_from
-                        stop = connection.id_to
-                        start = stop
+                        start = connection.id_to
+
 
             i += 1
 
-        print(f"traject: {traject}")
-        print(f"stations_dict: {stations_dict}")
-        print(f"dict: {dict}")
+        naam = stations_dict[1].name
+
+        # print(f"traject: {traject}")
+        # print(f"dict_trajects: {dict_trajects}")
+        # print(f"stations_dict[1]: {naam}")
+
+    # print(f"connection added: {connection}")
+    # print(f"Traject: {traject}\n"
+    #       f"Time: {traject.total_time}")
+
+    # Try again if traject too short
+    if traject.total_time < 60:
+        traject = traject_generator_depth(connections)
+
+    return traject
 
 
-    return
+if __name__ == "__main__":
+    traject_list = []
+    while len(traject_list) < 7:
+        traject_temp = traject_generator_depth(all_connections)
+        if traject_temp in traject_list:
+            print("Not added:")
 
-traject_generator_depth(all_connections)
-
-
-
-
-
-        #tried = 1
-        #while traject.total_time <= 120:
-            # using the Traject method add_connection, to add the connection at [j] in all_connections
-        #    if connections[index] not in traject.connections:
-        #        traject.add_connection(connections[index])
-        #        tried += 1
-            # if every connection has been tried:
-        #    if tried == length:
-                # if the total time of the current traject overceeds min_time use this traject.
-        #        if traject.total_time >= min_time:
-        #            trajects[f"Traject {i}."] = (traject, traject.total_time)
-        #            total_minutes.append(traject.total_time)
-        #            i += 1
-                    # add connections in this traject to used_all, eventually the use through all 7 trajects will be counted
-        #            for conn in traject.connections:
-        #                used_all.append(conn)
-        #                if conn in critical_connections:
-        #                    used_critical.append(conn)
-        #            break
-                # if the traject is too short, start over.
-        #        else:
-        #            break
-
-    #p = len(used_critical) / len(critical_connections)
-    #t = len(trajects.keys())
-    #total_minutes = sum(total_minutes)
-    #K = p*10000 - (t*20 + total_minutes/10)
-
-    #return(K)
-
-    # print(f"Greedy approach trajects output: {trajects} \n")
-    # # Counter lists the use of all connections
-    # counter = collections.Counter(used_all)
-    # print(f"Counter: {counter}\n")
-
-
-#K_distribution = []
-#for i in range(100):
-#    K = traject_generator_greedy(all_connections, 7, 90)
-#    K_distribution.append(K)
-
-#print(K_distribution)
-#plt.hist(K_distribution, bins='auto')  # arguments are passed to np.histogram
-#plt.title("K spread - 100 iterations - 6 trajects, 90 minutes max")
-#plt.show()
-
-
-
-
-
-
-# K de kwaliteit van de lijnvoering is, p de fractie van de bereden kritieke verbindingen (dus tussen 0 en 1),
-# T het aantal trajecten en Min het aantal minuten in alle trajecten samen.
+        # print(traject_list)
+        else:
+            traject_list.append(traject_temp)
+        print(f"Traject created: {traject_temp}")
+    print(f"K value:{K_calculator(traject_list)}")
